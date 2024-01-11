8fc63e7f13e903f15d9df93a8d429dd4606dbb45
Наталья Никитина <153123991+NataliSnail@users.noreply.github.com>
2024-01-11 12:04:07 +0300

    Update tests_authorization.py

--- tests/tests_authorization.py
+++ tests/tests_authorization.py
@@ -6,9 +6,10 @@ from Sprint.constants import Constants
 from Sprint.locators import Locators
 import time
 
+
 class TestAuthorization:
     def test_click_button_enter_to_account(self,driver):
-        '''кнопка "Войти в аккаунт"'''
+        '''авторизация через кнопку "Войти в аккаунт"'''
         driver.find_element(*Locators.ENTER_TO_ACCOUNT).click()
         driver.find_element(*Locators.EMAIL_FIELD).send_keys(Constants.TEST_EMAIL)
         driver.find_element(*Locators.PASSWORD_FIELD).send_keys(Constants.PASSWORD)
@@ -18,15 +19,11 @@ class TestAuthorization:
         driver.quit()
 
 
-    def test_click_button_personal_account(self, driver):
-        '''кнопка "Личный кабинет"'''
-        driver.find_element(*Locators.PERSONAL_ACCOUNT).click()
-        driver.find_element(*Locators.EMAIL_FIELD).send_keys(Constants.TEST_EMAIL)
-        driver.find_element(*Locators.PASSWORD_FIELD).send_keys(Constants.PASSWORD)
-        driver.find_element(*Locators.SUBMIT_BUTTON).click()
+    def test_click_button_personal_account(self, login):
+        '''авторизация через кнопку "Личный кабинет"'''
         # использую элемент "Оформить заказ" как подтверждение, что авторизация пройдена, т.к. без авторизации он недоступен
-        assert WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((Locators.CHECKOUT)))
-        driver.quit()
+        assert WebDriverWait(login, 10).until(expected_conditions.visibility_of_element_located((Locators.CHECKOUT)))
+
 
     def test_click_forgot_password(self):
         '''вход через кнопку "Восстановить пароль" в форме восстановления пароля'''
@@ -38,8 +35,7 @@ class TestAuthorization:
         WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((Locators.PASSWORD_FIELD)))
         driver.find_element(*Locators.PASSWORD_FIELD).send_keys(Constants.PASSWORD)
         driver.find_element(*Locators.CODE_RECOVERY_FIELD).send_keys(Constants.CODE_RECOVERY_PASSWORD)
-        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((Locators.SAVE_BUTTON)))
-        driver.find_element(*Locators.SAVE_BUTTON).click()
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((Locators.SAVE_BUTTON))).click()
         assert driver.current_url == 'https://stellarburgers.nomoreparties.site/reset-password'
         driver.quit()
 
