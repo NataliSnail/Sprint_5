baeb1bb5558f47d44bf80a0c0d3b41b4c2554ef4
Наталья Никитина <153123991+NataliSnail@users.noreply.github.com>
2024-01-11 12:02:30 +0300

    Update test_click_to_personal_account.py

--- tests/test_click_to_personal_account.py
+++ tests/test_click_to_personal_account.py
@@ -9,12 +9,7 @@ import time
 '''переход по клику на «Личный кабинет»'''
 
 class TestPersonalAccount:
-    def test_go_to_personal_account(self,driver):
-        driver.find_element(*Locators.PERSONAL_ACCOUNT).click()
-        driver.find_element(*Locators.EMAIL_FIELD).send_keys(Constants.TEST_EMAIL)
-        driver.find_element(*Locators.PASSWORD_FIELD).send_keys(Constants.PASSWORD)
-        driver.find_element(*Locators.SUBMIT_BUTTON).click()
-        driver.find_element(*Locators.PERSONAL_ACCOUNT).click()
-        assert WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((Locators.PROFILE)))
-        driver.quit()
+    def test_go_to_personal_account(self,login):
+        WebDriverWait(login, 10).until(expected_conditions.visibility_of_element_located(Locators.PERSONAL_ACCOUNT)).click()
+        assert WebDriverWait(login, 10).until(expected_conditions.visibility_of_element_located((Locators.PROFILE)))
 
