1172e68710b3aee270dcd8e052e7a7e192dcbd04
Наталья Никитина <153123991+NataliSnail@users.noreply.github.com>
2024-01-15 13:20:02 +0300

    Add files via upload

--- tests/test_click_to_personal_account.py
+++ tests/test_click_to_personal_account.py
@@ -2,9 +2,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions
-from Sprint.constants import Constants
-from Sprint.locators import Locators
-import time
+from constants import Constants
+from locators import Locators
+
 
 '''переход по клику на «Личный кабинет»'''
 
@@ -12,4 +12,4 @@ class TestPersonalAccount:
     def test_go_to_personal_account(self,login):
         WebDriverWait(login, 10).until(expected_conditions.visibility_of_element_located(Locators.PERSONAL_ACCOUNT)).click()
         assert WebDriverWait(login, 10).until(expected_conditions.visibility_of_element_located((Locators.PROFILE)))
-
+        
--- tests/test_logout.py
+++ tests/test_logout.py
@@ -1,14 +1,19 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
-from Sprint.constants import Constants
-from Sprint.locators import Locators
-import time
+from constants import Constants
+from locators import Locators
+
 
 
 class TestLogout:
     def test_logout(self,login):
         WebDriverWait(login, 10).until(expected_conditions.visibility_of_element_located(Locators.PERSONAL_ACCOUNT)).click()
         WebDriverWait(login, 10).until(expected_conditions.visibility_of_element_located((Locators.LOGOUT))).click()
-        assert WebDriverWait(login, 10).until(expected_conditions.visibility_of_element_located((Locators.PAGE_LOGIN)))
+        assert WebDriverWait(login, 10).until(expected_conditions.visibility_of_element_located((Locators.SUBMIT_AUTHORIZATION)))
+
+
+
+
+
 
